8c43af5cd45fd7bc385e15646ab993a0f84b3d50
basakstuff <basaker98@gmail.com>
2021-01-08 17:41:09 +0300

    wikibook scrap

--- scrap.py
+++ scrap.py
@@ -8,18 +8,6 @@ from requests.exceptions import ConnectionError
 import urllib.request
 from bs4 import BeautifulSoup
 
-##### Projenin çözdüğü problemlerin farklı durumları varsa,
-# yazdığınız kodun her zaman o durumları karşılayabilecek şekilde olması tercih edilmelidir.
-# Fakat bu projede kitabın içeriğine tek sayfa ile erişebildiğimizi kabul edeceğiz.
-# Yani programın Wikibooks’taki bitmiş olan kitaplarda çalışması yeterlidir
-# ve bu noktada printable versiyonunun hepsinde olduğu varsayılmıştır.
-# Eğer bir kitabın printable versiyonu yok ise, programınızın o kitap için çalışmasına gerek yok.########
-
-## Önemli not: Your program should ask the user, how many word frequencies they wish to see (in this
-# document, as you can see, 20 words per list has been shown). You can take 20 as your default number
-# if the user does not enter a specific number they wish to use.
-#### Bu programda eksik, kullanıcının kaç frequencies istediği sorulmalı.
-
 
 book_1 = input("Enter the first book name: ")
 book_1 = book_1.replace(" ", "_")
